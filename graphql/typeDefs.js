0fd371dd860bdace4509f2edaeb2a91cf4a0d7a5
visheshdvn <vishesh.dh1@gmail.com>
2021-09-09 02:28:59 +0530

    post create/delete API done

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -25,10 +25,13 @@ module.exports = gql`
   # Queries
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   # Mutations
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `;
--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers/index");
 const { MONGODB } = require("./config");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
 
 mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
-  return server.listen({ port: 5003 }).then((res) => {
+  return server.listen({ port: 5000 }).then((res) => {
     console.log("Server running at", res.url);
   });
 });
